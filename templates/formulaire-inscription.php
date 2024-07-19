dacf7da5db4e6cdcb7eb723a95318e903173f1c0
f-simard <e2396414@cmaisonneuve.qc.ca>
2024-07-19 11:18:07 -0400

    bordure autour des champs remplissables

--- templates/formulaire-inscription.php
+++ templates/formulaire-inscription.php
@@ -1,7 +1,6 @@
 <div class="iil-modale" style="background-color:<?php echo esc_attr( $iil_modale_params->couleur_bg ); ?> ; color:<?php echo esc_attr( $iil_modale_params->couleur_txt ); ?>">
 	<div class="iil-modale__boutonFermer">&#10006;</div>
-	<form action="" method="post" class="iil-formulaire__inscription" style="background-color:<?php echo esc_attr( $iil_modale_params->couleur_bg ); ?> ;
-	 color:<?php echo esc_attr( $iil_modale_params->couleur_txt ); ?>">
+	<form action="" method="post" class="iil-formulaire__inscription" style="background-color:<?php echo esc_attr( $iil_modale_params->couleur_bg ); ?> ; color:<?php echo esc_attr( $iil_modale_params->couleur_txt ); ?>">
 		<section class="ill-formulaire__section" data-panneau=0 >
 			<h2 style="color:<?php echo esc_attr( $iil_modale_params->couleur_txt ); ?>"><?php echo esc_html( $iil_modale_params->titre ); ?></h2>
 			<div class="ill-formulaire_input">
